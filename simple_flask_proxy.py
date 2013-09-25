ba9accb402f9783143dadf90928b25fcc2d89259
lowrey <lowrey@users.noreply.github.com>
2013-09-25 16:08:49 -0500

    Update simple_flask_proxy.py

--- simple_flask_proxy.py
+++ simple_flask_proxy.py
@@ -9,7 +9,6 @@ app = Flask(__name__)
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
-    #dest = url.geturl().replace(url.hostname, 'secure.authorize.net')
     FORWARD_HEADER = 'Forwarding-Address'
     # prepare header for foward request
     hdr = header_to_dict(request.headers)
@@ -19,12 +18,10 @@ def catch_all(path):
         fwd_addr = hdr[FORWARD_HEADER]
         hdr.pop(FORWARD_HEADER, None)
     if not fwd_addr:
-        fwd_addr = 'lowrey.me'
-        # return "Fowarding Address not set", 400
+        return "Fowarding Address not set", 400
     # change destination url
     url = urlparse(request.url)
     dest = url.geturl().replace(url.netloc, fwd_addr)
-    #print 'Dest: {}, {}, {}'.format(dest, request.method, str(hdr))
     r = requests.request(request.method, dest,
                          data=request.data,
                          headers=hdr)
@@ -42,16 +39,6 @@ def header_to_dict(headers):
             ret[k] = v
     return ret
 
-
-def print_request(req):
-    print "REQUEST"
-    print "======="
-    print "url = " + str(req.url)
-    print "json = " + str(req.json)
-    print "args = " + str(req.args)
-    print "data = " + str(req.data)
-    print "values = " + str(req.values)
-    print "headers = " + str(req.headers)
 
 
 if __name__ == '__main__':
